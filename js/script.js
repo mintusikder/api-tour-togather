e4b72daeb89d4e137b3e568993b7889650e8cfb8
mintusikder <mintu141617@gmail.com>
2023-02-26 14:01:18 +0600

    added-header

--- js/script.js
+++ js/script.js
@@ -5,13 +5,22 @@ const loadAllData =()=>{
 const showAllData =(countries)=>{
     const countryContainer =document.getElementById('countries-info')
     countries.forEach(country => {
-        console.log(country.flags)
+        console.log(country)
         const div =document.createElement('div')
         div.innerHTML=`
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card w-full  bg-base-100 shadow-2xl ">
   <figure class="px-10 pt-10">
     <img src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
   </figure>
+  <div class="card-body items-center text-center">
+  <h2 class="card-title">${country.name.common}</h2>
+  <p>Name: ${country.population}</p>
+  <p>Timezones: ${country.timezones}</p>
+  <p>Capital: ${country.capital}</p>
+  <div class="card-actions">
+    <button class="btn btn-primary">Details</button>
+  </div>
+</div>
   </div>
         `
         countryContainer.appendChild(div)
