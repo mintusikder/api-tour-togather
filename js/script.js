ef18226a6403eb122f39083615b6c422328e75bf
mintusikder <mintu141617@gmail.com>
2023-02-26 14:56:21 +0600

    loadAllData

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+// let addData
 const loadAllData =()=>{
     fetch('https://restcountries.com/v3.1/all').then(res=>res.json())
-    .then(data => showAllData(data))
+    // addData=data
+    .then(data => showAllData(data.slice(0,9)))
 }
+// console.log(addData)
 const showAllData =(countries)=>{
     const countryContainer =document.getElementById('countries-info')
+    countryContainer.innerHTML=""
     countries.forEach(country => {
-        console.log(country)
+        // console.log(country)
         const div =document.createElement('div')
         div.innerHTML=`
         <div class="card w-full  bg-base-100 shadow-2xl ">
@@ -18,13 +22,24 @@ const showAllData =(countries)=>{
   <p>Timezones: ${country.timezones}</p>
   <p>Capital: ${country.capital}</p>
   <div class="card-actions">
-    <button class="btn btn-primary">Details</button>
+    <button onClick="showDetails('${country.cca2}')" class="btn btn-primary">Details</button>
   </div>
 </div>
   </div>
         `
         countryContainer.appendChild(div)
     });
-
+}
+const showDetails =(id)=>{
+    // console.log(id)
+    const URL =`https://restcountries.com/v3.1/alpha/${id}`
+    fetch(URL).then((res)=>res.json())
+    .then(data => console.log(data))
 }
 loadAllData()
+
+const showAllDataTogether=()=>{
+    fetch('https://restcountries.com/v3.1/all').then(res=>res.json())
+    // addData=data
+    .then(data => showAllData(data))
+}
